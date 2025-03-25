eb149e6bbff500dcf939dd6753fd0e048ae3cc21
JJSzymanski <jedrzej.szymanski@googlemail.com>
2025-03-25 10:01:30 +0100

    Add files via upload

---
+++ script_ENA_get_data.sh
@@ -0,0 +1,13 @@
+# Get the data
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/097/SRR15244997/SRR15244997_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/010/SRR15245010/SRR15245010_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/098/SRR15244998/SRR15244998_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/008/SRR15245008/SRR15245008_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/097/SRR15244997/SRR15244997_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/007/SRR15245007/SRR15245007_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/013/SRR15245013/SRR15245013_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/010/SRR15245010/SRR15245010_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/098/SRR15244998/SRR15244998_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/013/SRR15245013/SRR15245013_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/008/SRR15245008/SRR15245008_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/007/SRR15245007/SRR15245007_2.fastq.gz
